6bf914277d4df9fb879a8b8d6444cb0d4650df15
Jamone Kelly <xg.monie089@gmail.com>
2018-03-29 14:30:02 -0400

    initial commit

---
+++ Bus Tracker/OnboardController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Bus Tracker
+//
+//  Created by Jamone Kelly on 3/29/18.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class OnboardController: UIViewController {
+    @IBOutlet weak var onboardLabel: UILabel!
+    @IBOutlet weak var adminButton: UIButton!
+    @IBOutlet weak var signinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignInAction(_ sender: Any) {
+        self.performSegue(withIdentifier: "onboard2signin", sender: self)
+    }
+    
+
+}
+
